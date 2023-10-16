d75e116a4e9d29c67fc2b7116359e3cfc4f6839c
WanNurAdila <43770889+WanNurAdila@users.noreply.github.com>
2023-10-16 12:34:33 +0800

    first commit for mason bricks

---
+++ widget/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}.dart
@@ -0,0 +1,11 @@
+// import 'package:flutter/material.dart';
+
+// class {{filename.pascalCase()}} extends StatelessWidget {
+
+//   // const {{filename.pascalCase()}}({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const SizedBox();
+//   }
+// }
