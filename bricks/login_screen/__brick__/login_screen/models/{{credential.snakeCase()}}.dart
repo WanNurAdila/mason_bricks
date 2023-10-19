7c7945098fdc892af77bd07c3490779cc854ce2e
WanNurAdila <43770889+WanNurAdila@users.noreply.github.com>
2023-10-19 17:13:20 +0800

    update regex for email

--- bricks/login_screen/__brick__/login_screen/models/{{credential.snakeCase()}}.dart
+++ bricks/login_screen/__brick__/login_screen/models/{{credential.snakeCase()}}.dart
@@ -4,10 +4,26 @@ enum {{credential.pascalCase()}}ValidationError { empty }
 
 class {{credential.pascalCase()}} extends FormzInput<String, {{credential.pascalCase()}}ValidationError> {
   const {{credential.pascalCase()}}.pure() : super.pure('');
-  const {{credential.pascalCase()}}.dirty([String value = '']) : super.dirty(value);
+  const {{credential.pascalCase()}}.dirty([String value = '']) : super.dirty(value); 
 
   @override
   {{credential.pascalCase()}}ValidationError? validator(String? value) {
-    return value?.isNotEmpty == true ? null : {{credential.pascalCase()}}ValidationError.empty;
+
+    bool isEmail = {{credetial}} == 'email' ? true : false
+
+    {{#isEmail}}
+    String p =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+    RegExp regExp = new RegExp(p);
+
+      return regExp.hasMatch(email)  == true ? null :{{credential.pascalCase()}}ValidationError.empty
+
+    {{/credential}}
+    {{^isEmail}}
+      return value?.isNotEmpty == true ? null : {{credential.pascalCase()}}ValidationError.empty;
+    {{/isEmail}}
+
+    
   }
 }
